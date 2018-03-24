11043f91803619a11264e40bcc4fbe3e2c3ab923
SayGood-bye <BIRlz@users.noreply.github.com>
2018-03-24 19:54:16 +0800

    Update A_NEW_LDA.py

--- discriminantAnalysis/A_NEW_LDA.py
+++ discriminantAnalysis/A_NEW_LDA.py
@@ -16,9 +16,17 @@ def class_mean(dataSet,cls):
 def within_class_SW(cls,class_mean):
     m,n = cls.shape()
     S_W = np.zeros((m,m))
-    for feat in cls:
-        S_W += (feat)
+    for j in len(cls):
+        for i in range(len(cls[j][0]):
+            S_W += (cls[j][i] - class_mean[i])**2
+    return S_W
 
+def between_class_SW(dataSet,cls_list):
+    cls_mean = []
+    for j in cls_list:
+        cls_mean.append(class_mean(dataSet,cls))
+    return np.mean(cls_mean)
+                       
 dataSet = [[1,2,1],[2,3,1],[3,4,2],[4,5,2]]
 print(class_mean(dataSet,1))   
 print(class_mean(dataSet,2))   
